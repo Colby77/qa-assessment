4e08971c02f1d9ceccaffab5cd575b0dcda5d7d2
Colby Workman <cgw2319@gmail.com>
2021-11-19 10:31:57 -0700

    3 successful tests for utils.js

--- functions.test.ts
+++ functions.test.ts
@@ -2,4 +2,15 @@ const {shuffleArray} = require('./utils')
 
 describe('shuffleArray should', () => {
     // CODE HERE
+    test('shuffleArray function returns array', () => {
+        expect(typeof shuffleArray([1, 2, 3, 4])).toEqual('object')
+    })
+    test('shuffleArray should return an array of the same length as the one passed', () => {
+        expect(shuffleArray([1, 2, 3, 4]).length).toBe(4)
+    })
+    test('returned array should contain same numbers', () => {
+        [1, 2, 3, 4].forEach((element) => {
+            expect(shuffleArray([1, 2, 3, 4])).toContain(element)
+        })
+    })
 })
